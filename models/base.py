70564cd91a3e1f60ad2a743a6e0c73c3f1fee804
Batuhan Edgüer <batuhanedguer@gmail.com>
2023-05-31 00:22:57 +0300

    some unnecessary functions deleted

--- models/base.py
+++ models/base.py
@@ -2,49 +2,13 @@ from joblib import (
     dump,
     load,
 )
-from sklearn.metrics import (
-    mean_squared_error,
-    mean_absolute_error,
-    r2_score,
-)
 from sklearn.model_selection import GridSearchCV
-
-import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 import logging
 
 
 class BaseModelNN:
-    def print_metrics(self, y_true, y_pred, n_features):
-        print(f"MSE: {mean_squared_error(y_true, y_pred)}")
-        print(f"RMSE: {np.sqrt(mean_squared_error(y_true, y_pred))}")
-        print(f"MAE: {mean_absolute_error(y_true, y_pred)}")
-        r2 = r2_score(y_true, y_pred)
-        print(f"R^2: {r2}")
-        n = len(y_true)
-        adjusted_r2 = 1 - ((1 - r2) * (n - 1) / (n - n_features - 1))
-        print(f"Adjusted R^2: {adjusted_r2}")
-        mape = np.mean(np.abs((y_true - y_pred) / y_true)) * 100
-        print(f"MAPE: {mape}%")
-
-        fig, axes = plt.subplots(1, 2, figsize=(10, 5))
-
-        # plotting residual plots
-        axes[0].scatter(y_pred, y_pred - y_true)
-        axes[0].set_title("Residual plot")
-        axes[0].set_xlabel("Predicted values")
-        axes[0].set_ylabel("Residuals")
-
-        # Actual vs Predicted values plot
-        axes[1].scatter(y_true, y_pred)
-        axes[1].set_title("Actual vs Predicted values")
-        axes[1].set_xlabel("Actual values")
-        axes[1].set_ylabel("Predicted values")
-
-        plt.tight_layout()
-        plt.show()
-
     def save_model(self, filename):
         self.model.save(filename)
 
@@ -98,9 +62,9 @@ class BaseRegressor:
         ).fit(X_train, y_train)
 
     def log_cv_score(self):
-        cv_score = np.sqrt(-self.grid_search.best_score_)
-        self.logger.info(f"Best cross-validation score: {cv_score}")
+        cv_score = -self.grid_search.best_score_
+        self.logger.info(f"Best cross-validation score (MSE): {cv_score}")
 
     def log_validation_score(self, X_val, y_val):
-        val_score = np.sqrt(-self.grid_search.score(X_val, y_val))
-        self.logger.info(f"Validation score: {val_score}")
+        val_score = -self.grid_search.score(X_val, y_val)
+        self.logger.info(f"Validation score (MSE): {val_score}")
